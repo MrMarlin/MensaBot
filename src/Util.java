b65ebb06b5c4679c2a6a5d4aa85b2c355afc8354
MrMarlin <s.lehner28@gmail.com>
2018-03-01 20:40:56 +0100

    voting bug fixed and new classes introduced for readability

---
+++ src/Util.java
@@ -0,0 +1,74 @@
+import com.vdurmont.emoji.EmojiParser;
+import menu.MenuplanCrawler;
+import menu.MenuplanList;
+import org.telegram.telegrambots.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class Util {
+
+    public int weekday = 2;
+
+    public String generateEmojiString(int counter) {
+        String emojis = "";
+        for (int i = 0; i < counter; i++) {
+            emojis += EmojiParser.parseToUnicode(":point_up:");
+        }
+        return emojis;
+    }
+
+    public InlineKeyboardMarkup generateKeyboardMarkup() {
+        InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
+        List<InlineKeyboardButton> rowInline = new ArrayList<>();
+        List<InlineKeyboardButton> rowInline1 = new ArrayList<>();
+
+        rowInline.add(new InlineKeyboardButton().setText("Mensa").setCallbackData("Mensa"));
+        rowInline.add(new InlineKeyboardButton().setText("Bistro").setCallbackData("Bistro"));
+        rowInline1.add(new InlineKeyboardButton().setText("Extern").setCallbackData("Extern"));
+        rowInline1.add(new InlineKeyboardButton().setText("Selber mitgnoh..").setCallbackData("Selber"));
+
+        rowsInline.add(rowInline);
+        rowsInline.add(rowInline1);
+        markupInline.setKeyboard(rowsInline);
+
+        return markupInline;
+    }
+
+    public String generateEmojiDivider() {
+        String divider = "";
+        for (int i = 0; i < 17; i++) {
+            divider += ":fork_knife_plate: ";
+        }
+        return EmojiParser.parseToUnicode(divider);
+    }
+
+    public String generateMenuText() {
+        MenuplanList mensaMenuplanList = MenuplanCrawler.crawlMenuplans("Mensa");
+        MenuplanList bistroMenuplanList = MenuplanCrawler.crawlMenuplans("Bistro");
+        String resultString = "";
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        Calendar cal = Calendar.getInstance();
+
+        resultString += "<b>" + dateFormat.format(cal.getTime()) + "</b>\n\n";
+
+        for (int i = 0; i < 3; i++) {
+            resultString += "<b>Mensa: </b>\n" + mensaMenuplanList.get(weekday - 2).get(i).getTitle() + "\n" +
+                    mensaMenuplanList.get(weekday - 2).get(i).getDescription() + "\n\n";
+        }
+
+        resultString += generateEmojiDivider() + "\n";
+        resultString += "\n";
+
+        for (int i = 0; i < 2; i++) {
+            resultString += "<b>Bistro: </b>\n" + bistroMenuplanList.get(weekday - 2).get(i).getTitle() + "\n" +
+                    bistroMenuplanList.get(weekday - 2).get(i).getDescription() + "\n";
+        }
+        return resultString;
+    }
+}
---
+++ src/Voting.java
@@ -0,0 +1,60 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Voting {
+
+    private Map<Long, Vote> votes = new HashMap<>();
+    private Map<String, Integer> voteCounter = new HashMap<>();
+
+
+    public void init(){
+        voteCounter.put("Mensa", 0);
+        voteCounter.put("Bistro", 0);
+        voteCounter.put("Extern", 0);
+        voteCounter.put("Selber", 0);
+    }
+
+    public String voteHandler(long userId, String call_data){
+        Util util = new Util();
+        String answer = "";
+        Vote current_vote = new Vote(userId, call_data);
+        this.init();
+
+        if (!(votes.containsKey(userId))) {
+            if (call_data.equals("Mensa")) {
+                voteCounter.put("Mensa", voteCounter.get("Mensa") + 1);
+            } else if (call_data.equals("Bistro")) {
+                voteCounter.put("Bistro", voteCounter.get("Bistro") + 1);
+            } else if (call_data.equals("Extern")) {
+                voteCounter.put("Extern", voteCounter.get("Extern") + 1);
+            } else if (call_data.equals("Selber")) {
+                voteCounter.put("Selber", voteCounter.get("Selber") + 1);
+            }
+            votes.put(userId, current_vote);
+            votes.get(userId).setVoted(true);
+        } else if (!(votes.get(userId).getLocation().equals(call_data))) {
+            if (voteCounter.get(votes.get(userId).getLocation()) > 0) {
+                voteCounter.put(votes.get(userId).getLocation(), voteCounter.get(votes.get(userId).getLocation()) - 1);
+            }
+            votes.put(userId, current_vote);
+            voteCounter.put(votes.get(userId).getLocation(), voteCounter.get(votes.get(userId).getLocation()) + 1);
+            votes.get(userId).setVoted(true);
+        } else if (votes.get(userId).getLocation().equals(call_data)) {
+            if (votes.get(userId).isVoted()) {
+                voteCounter.put(votes.get(userId).getLocation(), voteCounter.get(votes.get(userId).getLocation()) - 1);
+                votes.get(userId).setVoted(false);
+            } else {
+                voteCounter.put(votes.get(userId).getLocation(), voteCounter.get(votes.get(userId).getLocation()) + 1);
+                votes.get(userId).setVoted(true);
+            }
+        }
+        System.out.println(voteCounter);
+
+        answer += "<b>------Abstimmung------</b>";
+        answer += "\nMensa: " + "\t" + util.generateEmojiString(voteCounter.get("Mensa")) +
+                "\nBistro: " + util.generateEmojiString(voteCounter.get("Bistro")) +
+                "\nExtern: " + util.generateEmojiString(voteCounter.get("Extern")) +
+                "\nSelber: " + util.generateEmojiString(voteCounter.get("Selber"));
+        return answer;
+    }
+}
